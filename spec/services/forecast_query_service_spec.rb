f6fab082e0fc1fd1cc66738f805bd7db22c547fd
Mesut Ilhaner <mesutcan.121@gmail.com>
2021-10-14 00:19:21 -0700

    Record the requests with VCR to reuse the responses in service specs

--- spec/services/forecast_query_service_spec.rb
+++ spec/services/forecast_query_service_spec.rb
@@ -3,34 +3,32 @@ require 'rails_helper'
 RSpec.describe ForecastQueryService, type: :model do
 
   describe "with zipcode" do
-    it "returns success with valid zipcode" do
-      subject = described_class.new(zipcode: "90049")
-      results = subject.call
+    let(:sucessful_result) { described_class.new(zipcode: "90049").call }
+    let(:failed_result) { described_class.new(zipcode: "00000").call }
+
+    it "returns success with valid zipcode", :vcr do
       # Test 6 results are returned
-      expect(results["cnt"]).to eq(6)
-      expect(results["cod"]).to eq("200")
+      expect(sucessful_result["cnt"]).to eq(6)
+      expect(sucessful_result["cod"].to_i).to eq(200)
     end
 
-    it "returns error with non-existing zipcode" do
-      subject = described_class.new(zipcode: "00000")
-      results = subject.call
-      expect(results["cod"]).to eq("404")
+    it "returns error with non-existing zipcode", :vcr do
+      expect(failed_result["cod"].to_i).to eq(404)
     end
   end
 
   describe "with latitude and longtitude" do
-    it "returns success with valid latitude and longtitude" do
-      subject = described_class.new(lat_long: ["34.0522", "-118.2437"])
-      results = subject.call
+    let(:sucessful_result) { described_class.new(lat_long: ["34.0522", "-118.2437"]).call }
+    let(:failed_result) { described_class.new(lat_long: ["", ""]).call }
+
+    it "returns success with valid latitude and longtitude", :vcr do
       # Test 6 results are returned
-      expect(results["cnt"]).to eq(6)
-      expect(results["cod"]).to eq("200")
+      expect(sucessful_result["cnt"]).to eq(6)
+      expect(sucessful_result["cod"].to_i).to eq(200)
     end
 
-    it "returns error with non-existing latitude and longtitude" do
-      subject = described_class.new(lat_long: ["", ""])
-      results = subject.call
-      expect(results["cod"]).to eq("400")
+    it "returns error with non-existing latitude and longtitude", :vcr do
+      expect(failed_result["cod"].to_i).to eq(400)
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,10 +16,9 @@
 require "vcr"
 
 VCR.configure do |c|
-  c.configure_rspec_metadata!
   c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   c.hook_into :webmock
-  c.allow_http_connections_when_no_cassette = true
+  c.configure_rspec_metadata!
 end
 
 RSpec.configure do |config|
