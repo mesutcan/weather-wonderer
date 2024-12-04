05d1044d6a15af2ee0c58ec99be751d60549c4fb
Sait Mesutcan Ilhaner <mesutcan.121@gmail.com>
2024-12-04 10:43:56 -0800

    Reduce repeated logic for constructing URLs and fetching data.
    - Reduce repeated logic for constructing URLs and fetching data.
    -  Improve separation of responsibilities by encapsulating HTTP request logic.
    - Add mechanisms to gracefully handle API errors or unexpected responses.

--- app/services/weather_query_service.rb
+++ app/services/weather_query_service.rb
@@ -1,27 +1,47 @@
 class WeatherQueryService
   include Virtus.model
-  CURRENT_WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
-  attribute :zipcode
-  attribute :lat_long
+  CURRENT_WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5/weather".freeze
+
+  attribute :zipcode, String
+  attribute :lat_long, Array
 
   def call
-    @call = zipcode.present? ? weather_by_zipcode : weather_by_lat_long
+    if zipcode.present?
+      fetch_weather(query_params: { zip: "#{zipcode},us" })
+    elsif lat_long.present?
+      fetch_weather(query_params: { lat: lat_long[0], lon: lat_long[1] })
+    else
+      raise ArgumentError, "Either zipcode or lat_long must be provided."
+    end
   end
 
   private
 
   def api_key
-    Rails.application.credentials[:open_weather][:api_key]
+    Rails.application.credentials.dig(:open_weather, :api_key) || raise("API key not configured.")
   end
 
-  def weather_by_zipcode
-    weather_url = "#{CURRENT_WEATHER_BASE_URL}?zip=#{zipcode},us&appid=#{api_key}&units=imperial"
-    JSON.parse(Net::HTTP.get(URI(weather_url)))
+  def fetch_weather(query_params:)
+    query_params[:appid] = api_key
+    query_params[:units] = "imperial"
+
+    response = Net::HTTP.get_response(uri_with_params(query_params))
+    handle_response(response)
   end
 
-  def weather_by_lat_long
-    weather_url = "#{CURRENT_WEATHER_BASE_URL}?lat=#{lat_long[0]}&lon=#{lat_long[1]}&appid=#{api_key}&units=imperial"
-    JSON.parse(Net::HTTP.get(URI(weather_url)))
+  def uri_with_params(params)
+    URI(CURRENT_WEATHER_BASE_URL).tap do |uri|
+      uri.query = URI.encode_www_form(params)
+    end
+  end
+
+  def handle_response(response)
+    case response
+    when Net::HTTPSuccess
+      JSON.parse(response.body)
+    else
+      raise "Failed to fetch weather data: #{response.message} (#{response.code})"
+    end
   end
 end
